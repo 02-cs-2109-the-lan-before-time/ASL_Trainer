6cb4e94d62d3ae22893ccca0db663fd082b3220e
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-28 14:03:43 -0600

    added medipipe hands setup

--- js/index.js
+++ js/index.js
@@ -2,41 +2,54 @@
 
 
 const body = document.body
-
+const videoRef = document.querySelector('video');
 let startButton = document.getElementById('train_button');
 startButton.addEventListener("click", startWebCam)
 
 var constraints = { video: { frameRate: { ideal: 10, max: 15 } } };
 function startWebCam(event) {
-    tf.tensor([1, 2, 3, 4]).print();
-    // startWebStream()
-    getMedia()
- 
-
+    getMedia(videoRef)
 
 }
 
-
-async function startWebStream() {
-    const videoElement = document.getElementById("web_cam")
-    videoElement.width = 640;
-    videoElement.height = 400;
-    
-   
-}
-
-async function getMedia() {
+async function getMedia(video) {
    let constraints = { audio: false, video: { width: 640, height: 400 } };
 
     navigator.mediaDevices.getUserMedia(constraints)
     .then(function(mediaStream) {
-      let video = document.querySelector('video');
+      
       video.srcObject = mediaStream;
       video.onloadedmetadata = function(e) {
         video.play();
       };
     })
-    .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+    .catch(function(err) { console.log(err.name + ": " + err.message); }); 
   }
 
+//mediaPipe
 
+function onResults(results){
+    console.log(results.multiHandLandmarks)
+}
+
+
+
+  const hands = new Hands({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  }});
+  hands.setOptions({
+    maxNumHands: 2,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+  hands.onResults(onResults);
+  
+  const camera = new Camera(videoRef, {
+    onFrame: async () => {
+      await hands.send({image: videoRef});
+    },
+    width: 1280,
+    height: 720
+  });
+//   camera.start();
