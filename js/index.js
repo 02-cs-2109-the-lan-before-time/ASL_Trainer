9c00095ead7f385a015bc6679f4d86eabedf704f
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-28 17:28:36 -0600

    built prompt for user classes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 
-
-
 const body = document.body
 const videoRef = document.querySelector('video');
 let startButton = document.getElementById('train_button');
 startButton.addEventListener("click", startWebCam)
+let labels = []
 
 var constraints = { video: { frameRate: { ideal: 10, max: 15 } } };
 function startWebCam(event) {
+    startButton.remove()
     getMedia(videoRef)
+    let labels = prompt("Enter Labels:",)
+    console.log(labels.split(','))
+    createLabelButtons()
+    
+    runCamera()
+}
 
+function createLabelButtons(labels){
+    let captureButton = document.createElement('button')
+    captureButton.innerHTML="Train Label A"
+    body.append(captureButton)
 }
 
 async function getMedia(video) {
@@ -29,10 +39,22 @@ async function getMedia(video) {
 //mediaPipe
 
 function onResults(results){
-    console.log(results.multiHandLandmarks)
+   let transformedData = []
+    let landMarks =  results.multiHandLandmarks
+ 
+    if(landMarks.length > 0){
+        let handOne = landMarks[0]
+         console.log(handOne)
+    }
+    //   console.log(transformedData)
+        
+    }
+    
+   
+
+function transformData(arr){
+   
 }
-
-
 
   const hands = new Hands({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
@@ -43,13 +65,22 @@ function onResults(results){
     minDetectionConfidence: 0.5,
     minTrackingConfidence: 0.5
   });
-  hands.onResults(onResults);
+  
   
   const camera = new Camera(videoRef, {
     onFrame: async () => {
       await hands.send({image: videoRef});
     },
-    width: 1280,
-    height: 720
+    width: 640,
+    height: 400
   });
-//   camera.start();
+
+function runCamera(){
+    camera.start()
+    setInterval(function(){ 
+        console.log("GO!")
+        hands.onResults(onResults);
+     }, 3000);
+    
+}
+ 
