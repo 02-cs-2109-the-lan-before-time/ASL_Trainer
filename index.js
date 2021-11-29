9657f41010cc7cf982ffbd6a31128f2a56d934b9
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-29 00:08:38 -0600

    got a sample output csv

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const csv = require('csv-parser')
+console.log(csv)
+
+let data = [
+        {
+        a: "2",
+        b: "2",
+        c: "1",
+    },
+    {
+        a: "5",
+        b: "1",
+        c: "9",
+
+    }
+]
+function writeToCSVFile(data) {
+    const filename = 'output.csv';
+    fs.writeFile(filename, extractAsCSV(data), err => {
+      if (err) {
+        console.log('Error writing to csv file', err);
+      } else {
+        console.log(`saved as ${filename}`);
+      }
+    });
+  }
+
+  function extractAsCSV(data) {
+    const header = ["Username, Password, Roles"];
+    const rows = data.map(d =>
+       `${d.a}, ${d.b}, ${d.c}`
+    );
+    return header.concat(rows).join("\n");
+  }
+
+
+
+  writeToCSVFile(data)
