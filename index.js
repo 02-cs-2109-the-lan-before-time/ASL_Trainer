08efd14ffae8f15c93e5d85ce39d73d61e4fa963
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-29 00:16:20 -0600

    made example csv file

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-const fs = require('fs')
-const csv = require('csv-parser')
-console.log(csv)
+const fs = require("fs");
+const csv = require("csv-parser");
 
 let data = [
-        {
-        a: "2",
-        b: "2",
-        c: "1",
-    },
-    {
-        a: "5",
-        b: "1",
-        c: "9",
-
-    }
-]
+  {
+    a: "2",
+    b: "2",
+    c: "1",
+  },
+  {
+    a: "5",
+    b: "1",
+    c: "9",
+  },
+];
 function writeToCSVFile(data) {
-    const filename = 'output.csv';
-    fs.writeFile(filename, extractAsCSV(data), err => {
-      if (err) {
-        console.log('Error writing to csv file', err);
-      } else {
-        console.log(`saved as ${filename}`);
-      }
-    });
-  }
+  const filename = "output.csv";
+  fs.writeFile(filename, extractAsCSV(data), (err) => {
+    if (err) {
+      console.log("Error writing to csv file", err);
+    } else {
+      console.log(`saved as ${filename}`);
+    }
+  });
+}
 
-  function extractAsCSV(data) {
-    const header = ["Username, Password, Roles"];
-    const rows = data.map(d =>
-       `${d.a}, ${d.b}, ${d.c}`
-    );
-    return header.concat(rows).join("\n");
-  }
+function extractAsCSV(data) {
+  const header = ["Username, Password, Roles"];
+  const rows = data.map((d) => `${d.a}, ${d.b}, ${d.c}`);
+  return header.concat(rows).join("\n");
+}
 
-
-
-  writeToCSVFile(data)
+writeToCSVFile(data);
