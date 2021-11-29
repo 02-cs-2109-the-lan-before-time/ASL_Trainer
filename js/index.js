2bf43626c83e64774cc0d61949d9a4d12a63bd96
Lauren Hale <lauren.m.hale7@gmail.com>
2021-11-28 23:47:21 -0600

    can save handlandmark data dynamically

--- js/index.js
+++ js/index.js
@@ -1,86 +1,115 @@
+const body = document.body;
+const videoRef = document.querySelector("video");
+let startButton = document.getElementById("train_button");
+startButton.addEventListener("click", start);
+const DataClass = new CaptureData([], {});
+let stopVidTimeOut;
+let curretClass;
 
-const body = document.body
-const videoRef = document.querySelector('video');
-let startButton = document.getElementById('train_button');
-startButton.addEventListener("click", startWebCam)
-let labels = []
 
-var constraints = { video: { frameRate: { ideal: 10, max: 15 } } };
-function startWebCam(event) {
-    startButton.remove()
-    getMedia(videoRef)
-    let labels = prompt("Enter Labels:",)
-    //call makeLabel class method
-    createLabelButtons()
-    
-    runCamera()
+const hands = new Hands({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  },
+});
+hands.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5,
+});
+
+function start(event) {
+  startButton.remove();
+  getMedia(videoRef);
+  let labels = prompt("Enter Labels:");
+  DataClass.makeLabels(labels);
+  createLabelButtons(DataClass.labels);
+  console.log(DataClass);
 }
 
-function createLabelButtons(labels){
-    let captureButton = document.createElement('button')
-    captureButton.innerHTML="Train Label A"
-    body.append(captureButton)
+function createLabelButtons(labels) {
+  for (let key in labels) {
+    let captureButton = document.createElement("button");
+    captureButton.innerHTML = `Train Label ${key}`;
+    captureButton.setAttribute("id", labels[key]);
+    captureButton.classList.add("training_buttons");
+    captureButton.onclick = function (e) {
+     setClass(this.id);
+     collectData()
+    };
+    body.append(captureButton);
+  }
+  let stopButton = document.createElement("button");
+  stopButton.innerHTML="stop"
+  stopButton.addEventListener('click', stopCollection)
+  body.append(stopButton)
+}
+
+function stopCollection(){
+    console.log("stopped")
+    console.log(DataClass.dataSet)
 }
 
 async function getMedia(video) {
-   let constraints = { audio: false, video: { width: 640, height: 400 } };
+  let constraints = { audio: false, video: { width: 640, height: 400 } };
 
-    navigator.mediaDevices.getUserMedia(constraints)
-    .then(function(mediaStream) {
-      
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (mediaStream) {
       video.srcObject = mediaStream;
-      video.onloadedmetadata = function(e) {
+      video.onloadedmetadata = function (e) {
         video.play();
       };
     })
-    .catch(function(err) { console.log(err.name + ": " + err.message); }); 
-  }
+    .catch(function (err) {
+      console.log(err.name + ": " + err.message);
+    });
+}
 
 //mediaPipe
 
-function onResults(results){
-   let transformedData = []
-    let landMarks =  results.multiHandLandmarks
- 
-    if(landMarks.length > 0){
-        let handOne = landMarks[0]
-         console.log(handOne)
-    }
-    //   console.log(transformedData)
-        
-    }
-    
-   
-
-function transformData(arr){
-   
+function onResults(results) {
+    let landMarks = results.multiHandLandmarks
+    if (landMarks.length > 0 ) {
+            DataClass.addFramesToData(landMarks[0], curretClass);
+            console.log(landMarks[0])
+      }
+    //   console.log(DataClass.dataSet)
 }
 
-  const hands = new Hands({locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-  }});
-  hands.setOptions({
-    maxNumHands: 2,
-    modelComplexity: 1,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
-  });
-  
-  
-  const camera = new Camera(videoRef, {
-    onFrame: async () => {
-      await hands.send({image: videoRef});
-    },
-    width: 640,
-    height: 400
+
+
+
+const camera = new Camera(videoRef, {
+  onFrame: async () => {
+    await hands.send({ image: videoRef });
+  },
+  width: 640,
+  height: 400,
+});
+
+function runCamera() {
+  camera.start();
+}
+function stopStreamedVideo(videoElem) {
+  const stream = videoElem.srcObject;
+  const tracks = stream.getTracks() || null;
+
+  tracks.forEach(function (track) {
+    track.stop();
   });
 
-function runCamera(){
-    camera.start()
-    setInterval(function(){ 
-        console.log("GO!")
-        hands.onResults(onResults);
-     }, 3000);
+  videoElem.srcObject = null;
+}
+
+function setClass(buttonId){
+    curretClass = buttonId
+}
+
+function collectData() {
+     runCamera();
     
 }
- 
+
+hands.onResults(onResults);
