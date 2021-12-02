77b43a704c1785542016953ce74ac34a27cf5fe9
Boxu Fan <boxufan@gmail.com>
2021-12-02 12:41:49 -0600

    resolve conflict

--- js/trainer.js
+++ js/trainer.js
@@ -1,12 +1,14 @@
+// load and format data
 async function load_process_data() {
 	let AD_df = await dfd.read_csv(
 		'https://docs.google.com/spreadsheets/d/1nBP6rdesizWW0hamkI11Dk1eB3uJz2G5wglAq6pT5To/export?format=csv&gid=0'
 	);
 
-	const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	//array of letters a-z
+	const letters = 'abcd'.split('');
 
+	//converts label to a one hot tensor
 	function convertCat(label) {
-		const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 		const zeroes = new Array(letters.length).fill(0);
 		const indexNum = letters.indexOf(label);
 		const outputArr = [...zeroes];
@@ -14,61 +16,15 @@ async function load_process_data() {
 		outputArr[indexNum] = 1;
 
 		return outputArr;
-	} //[1,00000...] [01000...]
-
-	for (let i = 0; i < letters.length; i++) {
-		AD_df = await AD_df.replace(letters[i], convertCat(letters[i]), {
-			columns: ['63'],
-		});
 	}
 
-	//AD_df.print();
-
-	//convert to tensor
-	let tf_tensor = AD_df.tensor;
-	console.log(tf_tensor.dtype);
-	console.log('tf tensor', tf_tensor);
-	tf_tensor.print();
-
-	function shuffle(tf_tensor) {
-		// Wrapping these calculations in a tidy will dispose any
-		// intermediate tensors.
-
-		return tf.tidy(() => {
-			// Step 1. Shuffle the data
-			const data = tf.util.shuffle(tf_tensor);
-
-			// Step 2. Convert data to Tensor
-			const inputTensor = data[0];
-			const labelTensor = data[1];
-			console.log('inputTensor', inputTensor);
-
-			//Step 3. Normalize the data to the range 0 - 1 using min-max scaling
-			const inputMax = inputTensor.max();
-			const inputMin = inputTensor.min();
-			const labelMax = labelTensor.max();
-			const labelMin = labelTensor.min();
-
-			const normalizedInputs = inputTensor
-				.sub(inputMin)
-				.div(inputMax.sub(inputMin));
-			const normalizedLabels = labelTensor
-				.sub(labelMin)
-				.div(labelMax.sub(labelMin));
-
-			return {
-				inputs: normalizedInputs,
-				labels: normalizedLabels,
-				// Return the min/max bounds so we can use them later.
-				inputMax,
-				inputMin,
-				labelMax,
-				labelMin,
-			};
-		});
-	}
-
-	shuffle();
+	// for (let i = 0; i < letters.length; i++) {
+	// 	AD_df = await AD_df.replace(letters[i], convertCat(letters[i]), {
+	// 		columns: ['63'],
+	// 	});
+	// }
+	console.log(AD_df);
+	return AD_df;
 }
 
 /*
@@ -94,33 +50,122 @@ y_train [[100],[]]
 the model will return one of our three cat arrays
 */
 
-load_process_data();
 //Define model architechture
-// async function buildModel() {
-// 	const model = tf.sequential();
-// 	// model.add(
-// 	// 	// tf.layers.lstm({
-// 	// 	// 	units: 64,
-// 	// 	// 	returnSequences: true,
-// 	// 	// 	activation: 'relu',
-// 	// 	// 	inputShape: [60, 64],
-// 	// 	// })
-// 	// );
-// 	//single input layer
-// 	model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
-// 	//single output layer
-// 	model.add(tf.layers.dense({units: 1, useBias: true}));
+function buildModel() {
+	const model = tf.sequential();
 
-// 	//dense layer
-// 	//    model.add(tf.layers.dense({units:64, })
-// 	model.summary();
-// 	model.compile({
-// 		loss: 'categoricalCrossentropy',
-// 		optimizer: 'Adam',
-// 		metrics: ['categoricalAccuracy'],
-// 	});
+	//single input layer with 64 weights for each of the 63 input features of the data
+	model.add(
+		tf.layers.dense({
+			inputShape: [63],
+			units: 64,
+			activation: 'relu',
+			useBias: true,
+		})
+	);
+	//layer
+	model.add(tf.layers.dense({units: 64, activation: 'relu'}));
+	//single output layer with [number of labels] of units
+	//softmax normalizes output to give probabilities
+	model.add(tf.layers.dense({units: 4, activation: 'softmax', useBias: true}));
 
-// 	// await model.fit(xs, ys, {epochs: 1000});
-// }
+	return model;
+}
 
-// buildModel();
+//convert input data to tensors
+function convertToTensor(data) {
+	//tidy disposes of any intermediate tensors
+	return tf.tidy(() => {
+		//shuffle data so model isn't learning things that are dependent on the order data is being fed in, and model isn't sensitive to the structure of subgroups
+		tf.util.shuffle(data);
+
+		//convert data to tensor: input tensor for coordinates, labels tensor for labels (a,b,...z...1,2,..10)
+
+		//split data so the 63 coordinates are in an inputs array and the 64th column (labels) are in the labels array
+		//inputs shape: num_of_datapoints x 63
+		//const inputs = data.map(d => d.slice(0,63));
+
+		const inputs = data.iloc({columns: ['0:63']});
+		//labels shape: num_of_datapoints x lenth_of_one_hot
+		//const labels = data.map(d => d[63]);
+		const oldlabels = data.iloc({columns: [63]});
+		//const labels = new oldlabels.OneHotEncoder();
+		let encode = new dfd.OneHotEncoder();
+		encode.fit(data[63]);
+		console.log(encode);
+		console.log(inputs.shape, 'HGFHGFHGFGHFHFH');
+		let labels = encode.transform(data[63].values);
+		labels.print();
+
+		//inputs.length is the number of examples, 63 is the number of features per examples
+		const inputTensor = inputs.tensor;
+		const labelTensor = labels.tensor;
+
+		//normalize data using min-max scaling to the range 0-1 (best practice)
+		const inputMax = inputTensor.max();
+		const inputMin = inputTensor.min();
+		const labelMax = labelTensor.max();
+		const labelMin = labelTensor.min();
+
+		const normalizedInputs = inputTensor
+			.sub(inputMin)
+			.div(inputMax.sub(inputMin));
+		const normalizedLabels = labelTensor
+			.sub(labelMin)
+			.div(labelMax.sub(labelMin));
+
+		//return the data and normalization bounds
+		return {
+			inputs: normalizedInputs,
+			labels: normalizedLabels,
+			//return min/max bounds for later use
+			inputMax,
+			inputMin,
+			labelMax,
+			labelMin,
+		};
+	});
+}
+
+//train model
+async function trainModel(model, inputs, labels) {
+	//prepare model for training
+	model.compile({
+		//adam optimizer is "effective in practice and requires no configuration"
+		optimizer: tf.train.adam(),
+		//meanSquareError compares the model's predictions with the actual values
+		loss: tf.losses.meanSquaredError,
+		metrics: ['mse'],
+	});
+
+	//batchSize is the size of the data subsets the model will see on each iteration of training
+	const batchSize = 32;
+	//epochs is the number of times the model will look at the entire dataset
+	const epochs = 50;
+
+	//start the train loop
+	return await model.fit(inputs, labels, {
+		batchSize,
+		epochs,
+		callbacks: tfvis.show.fitCallbacks(
+			{name: 'Training Performance'},
+			['loss', 'mse'],
+			{height: 200, callbacks: ['onEpochEnd']}
+		),
+	});
+}
+
+async function run() {
+	//gets data
+	const data = await load_process_data();
+	//get model
+	const model = buildModel();
+	//converts data for tensors for training
+	const tensorData = convertToTensor(data);
+	const {inputs, labels} = tensorData;
+	//train model
+	await trainModel(model, inputs, labels);
+	console.log('Done Training');
+}
+
+run();
